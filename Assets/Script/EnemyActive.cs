a4fccaf7d6c5bbf2965ff29bdd885764bd0caf19
NOISTIC <112389075+NOISTIC@users.noreply.github.com>
2025-04-12 12:49:17 +0700

    Enemy Patrol

--- Assets/Script/EnemyActive.cs
+++ Assets/Script/EnemyActive.cs
@@ -2,8 +2,39 @@ using UnityEngine;
 
 public class EnemyActive : MonoBehaviour
 {
+    [Header("Health")]
     public float health = 100f;
+    [Header("Patrolling")]
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public int Speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        targetPoint = 0;
+    }
+    private void Update()
+    {
+        moving();
+    }
+
+    public void moving()
+    {
+        if (transform.position == patrolPoints[targetPoint].position)
+        {
+            increaseTarget();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, Speed * Time.deltaTime);
+    }
+
+    public void increaseTarget()
+    {
+        targetPoint++;
+        if (targetPoint >= patrolPoints.Length)
+        {
+            targetPoint = 0;
+        }
+    }
     public void TakeDamage(float amount)
     {
         health -= amount;
